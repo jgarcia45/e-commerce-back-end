b363f307eaac99a5249035254ffe1320f19a722c
Juan <jgarcia1545@gmail.com>
2022-05-11 15:37:38 -0700

    Added some comments and refactored the code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { CANCELLED } = require('dns');
+//const { CANCELLED } = require('dns');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
